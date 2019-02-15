7770ab9936069e7cfe92d02e9840a7421c6374f0
Christine Cua <cloudycua@gmail.com>
2019-02-15 18:03:32 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,21 @@
+class Driver {
+  constructor(name, startDate){
+    this.name = name;
+    this.startDate = new Date(startDate);     // sets as a date object
+  }
+
+  yearsExperienceFromBeginningOf(endDate) {
+    return endDate - this.startDate.getFullYear();       // getFullYear()
+  }
+}
+
+class Route {
+  constructor(beginningLocation, endingLocation) {
+    this.beginningLocation = beginningLocation;
+    this.endingLocation = endingLocation;
+  }
+
+  blocksTravelled() {
+
+  }
+}
